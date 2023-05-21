b0c7ddb3a32ed1b7a0c26ec3a8ca2238584967c2
Ringu <ringugeorge@gmail.com>
2023-05-21 22:13:46 +0530

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Mohan on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
